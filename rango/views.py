cfd66ea45a4464ac5dbfa44db1d824c16f5aefac
jrkay <jrkohlmeyer@gmail.com>
2014-01-02 16:34:09 -0800

    Chapter 4
    Media and Static folders created, templates updated

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("Rango says hello world!<p><a href='/rango/about'>About Page</a></p>")
+	context = RequestContext(request)
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	return render_to_response('rango/index.html', context_dict, context)
+	
 	
 def about(request):
-	return HttpResponse("Rango Says: Here is the about page.<p><a href='/rango'>Main Page</a></p>")
+	context = RequestContext(request)
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	return render_to_response('rango/about.html', context_dict, context)
